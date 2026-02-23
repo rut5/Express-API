6b1534fa3bd6f143db61c1f04eea94504b308960
Rut <rutwintzell@outlook.com>
2026-02-23 13:04:49 +0100

    Made some changes in package.json and installed nodemon

--- server.ts
+++ server.ts
@@ -1,1 +1,18 @@
 // npm run lint
+
+import express from 'express';
+
+// Initalize the Express app
+const app = express();
+
+const PORT = 3000;
+
+// Simple route 
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
