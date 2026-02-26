e83fc9277e647a2f681b59fcc140a20d8ff7cf1c
Rut <rutwintzell@outlook.com>
2026-02-26 16:18:44 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -16,3 +16,21 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
+// User data
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+  ];
+  res.json(users);
+});
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+  res.json({ message: 'User added successfully', user: newUser });
+});
